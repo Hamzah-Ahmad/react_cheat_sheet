745f305b24ac456c5b98d55ab48cb20a0820903d
unknown <ahmadhamzah500@gmail.com>
2019-11-24 17:43:37 -0600

    Changed title

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import {Container} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<BrowserRouter><Container><App /></Container></BrowserRouter>, document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <Container>
+      <App />
+    </Container>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
